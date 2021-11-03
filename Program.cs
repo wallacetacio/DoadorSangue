de383fd0afda8cbfac8792141e6ffde54be137ff
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-03 10:43:18 -0300

    Adiciona resolução ao exercício Doador Sangue ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DoadorSangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade;
+
+            Console.Write("Olá, usuário! Por favor, digite a sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            if(idade > 18 && idade < 67){
+                Console.Write("Você ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("pode ");
+                Console.ResetColor();
+                Console.Write("ser doador de ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("sangue");
+                Console.ResetColor();
+                Console.WriteLine("!");
+
+            }else
+            {
+                Console.Write("Você ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("não ");
+                Console.ResetColor();
+                Console.Write("pode ser doador de ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("sangue");
+                Console.ResetColor();
+                Console.WriteLine("!");
+
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# DoadorSangue
-Exercício Doador Sangue ETEC 2021
+## Exercício `DoadorSangue` ETEC 2021
+
+Solicite a idade do usuário. Avalie se ele pode ser doador de sangue (i.e. possui idade entre 18 e 67 anos). Informe o resultado da avaliação.
+
+Ex.:
+```
+Qual a sua idade: 35
+Você pode ser doador de sangue.
+```
+ou
+```
+Qual a sua idade: 12
+Você não pode ser doador de sangue.
+```
+ou
+```
+Qual a sua idade: 80
+Você não pode ser doador de sangue.
+```
